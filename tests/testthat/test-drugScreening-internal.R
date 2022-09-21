a1ace7b6e01c17f76eaf0a42bc27dd9301a74bf7
Astrid Deschenes <adeschen@hotmail.com>
2022-09-21 19:28:19 -0400

    Adding unit tests

--- tests/testthat/test-drugScreening-internal.R
+++ tests/testthat/test-drugScreening-internal.R
@@ -60,8 +60,6 @@ test_that("findOneDrugQuantile() must return expected results 02", {
 })
 
 
-
-
 test_that("findOneDrugQuantile() must return expected error when not enough data", {
 
     drug <- readRDS(test_path("fixtures", "OneDrugDemoFile.RDS"))
